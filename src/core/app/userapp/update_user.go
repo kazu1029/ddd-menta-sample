49943411cd3c187c1efc778e2eac1c5398363112
kazu1029 <imgnt10nxqu05@gmail.com>
2020-11-15 12:45:42 +0900

    add update_user app

---
+++ src/core/app/userapp/update_user.go
@@ -0,0 +1,52 @@
+package userapp
+
+import (
+	"github.com/kazu1029/ddd-menta-sample/src/core/domain/userdm"
+	"github.com/kazu1029/ddd-menta-sample/src/core/domain/vo"
+)
+
+type UpdateUserApp struct {
+	userRepo userdm.UserRepository
+}
+
+func NewUpdateUserApp(userRepo userdm.UserRepository) *UpdateUserApp {
+	return &UpdateUserApp{
+		userRepo: userRepo,
+	}
+}
+
+type UpdateUserRequest struct {
+	ID               uint32
+	UserName         string
+	Email            string
+	Password         string
+	SelfIntroduction string
+	SkillIDs         []uint32
+}
+
+type UpdateUserResponse struct {
+	ID               uint32
+	UserName         string
+	Email            string
+	SelfIntroduction string
+	SkillIDs         []uint32
+}
+
+func (app *UpdateUserApp) Exec(req *UpdateUserRequest) (*UpdateUserResponse, error) {
+	userID, err := vo.NewUserID(req.ID)
+	if err != nil {
+		return nil, err
+	}
+	user, err := app.userRepo.FindByID(userID)
+	if err != nil {
+		return nil, err
+	}
+	email, err := vo.NewEmail(req.Email)
+	if err != nil {
+		return nil, err
+	}
+	password, err := vo.NewPassword(req.Password)
+	if err != nil {
+		return nil, err
+	}
+}
