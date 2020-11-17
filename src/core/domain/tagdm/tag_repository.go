d0b9cae4b46c111680868d1ad97620aaf6067128
kazu1029 <imgnt10nxqu05@gmail.com>
2020-11-17 09:38:24 +0900

    update tag repository

--- src/core/domain/tagdm/tag_repository.go
+++ src/core/domain/tagdm/tag_repository.go
@@ -4,4 +4,5 @@ import "github.com/kazu1029/ddd-menta-sample/src/core/domain/vo"
 
 type TagRepository interface {
 	FindByID(vo.TagID) (*Tag, error)
+	FindByIDs([]vo.TagID) ([]*Tag, error)
 }
--- src/core/infra/repoimpl/tag_repository.go
+++ src/core/infra/repoimpl/tag_repository.go
@@ -12,9 +12,32 @@ func NewTagRepoImpl() *TagRepoImpl {
 }
 
 var (
-	tags = []tagdm.Tag{}
+	tags map[string]*tagdm.Tag
 )
 
+func init() {
+	tag1, _ := tagdm.NewTag(vo.TagID("id1"), "Go")
+	tag2, _ := tagdm.NewTag(vo.TagID("id2"), "PHP")
+	tag3, _ := tagdm.NewTag(vo.TagID("id3"), "AWS")
+	tag4, _ := tagdm.NewTag(vo.TagID("id4"), "JavaScript")
+	tag5, _ := tagdm.NewTag(vo.TagID("id5"), "GCP")
+	tags["id1"] = tag1
+	tags["id2"] = tag2
+	tags["id3"] = tag3
+	tags["id4"] = tag4
+	tags["id5"] = tag5
+}
+
 func (repo *TagRepoImpl) FindByID(tagID vo.TagID) (*tagdm.Tag, error) {
-	return nil, nil
+	return tags[tagID.Value()], nil
+}
+
+func (repo *TagRepoImpl) FindByIDs(tagIDs []vo.TagID) ([]*tagdm.Tag, error) {
+	var fetchedTags []*tagdm.Tag
+	for _, tagID := range tagIDs {
+		if _, ok := tags[tagID.Value()]; ok {
+			fetchedTags = append(fetchedTags, tags[tagID.Value()])
+		}
+	}
+	return fetchedTags, nil
 }
